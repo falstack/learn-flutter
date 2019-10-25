5708f1b109bc686e5703678df118985b7460ddd1
falstack <icesilt@outlook.com>
2019-10-25 15:13:12 +0800

    use scaffold

--- lib/pages/widgets/demo-02.dart
+++ lib/pages/widgets/demo-02.dart
@@ -3,10 +3,31 @@ import 'package:flutter/material.dart';
 class WidgetDemo02App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Center(
-      child: new Text(
-        'Hello, world!',
-        textDirection: TextDirection.ltr,
+    //Scaffold是Material中主要的布局组件.
+    return new Scaffold(
+      appBar: new AppBar(
+        leading: new IconButton(
+          icon: new Icon(Icons.menu),
+          tooltip: 'Navigation menu',
+          onPressed: null,
+        ),
+        title: new Text('Example title'),
+        actions: <Widget>[
+          new IconButton(
+            icon: new Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          ),
+        ],
+      ),
+      //body占屏幕的大部分
+      body: new Center(
+        child: new Text('Hello, world!'),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        tooltip: 'Add', // used by assistive technologies
+        child: new Icon(Icons.add),
+        onPressed: null,
       ),
     );
   }
